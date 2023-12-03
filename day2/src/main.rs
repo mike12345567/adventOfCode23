ebc294a9860fe2ac7c6be2d249452c8356fab848
Michael Drury <me@michaeldrury.co.uk>
2023-12-03 00:52:34 +0000

    Adding second part.

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -36,10 +36,16 @@ pub fn main() -> io::Result<()> {
   file.read_to_string(&mut contents)?;
   let lines: Vec<&str> = contents.lines().collect();
   let mut sum: u32 = 0;
+  let mut sum_powers: u32 = 0;
   let mut valid_games: u32 = 0;
   for line in lines {
     let parts: Vec<&str> = line.split(';').collect();
     let mut valid = true;
+    let mut min = Cubes {
+      blue: 0,
+      red: 0,
+      green: 0,
+    };
     for part in parts {
       let mut cubes = Cubes {
         blue: 0,
@@ -48,22 +54,36 @@ pub fn main() -> io::Result<()> {
       };
       if let Some(blue) = check_pattern(part, &BLUE) {
         cubes.blue = cubes.blue + blue;
+        if min.blue < blue {
+          min.blue = blue;
+        }
       }
       if let Some(red) = check_pattern(part, &RED) {
         cubes.red = cubes.red + red;
+        if min.red < red {
+          min.red = red;
+        }
       }
       if let Some(green) = check_pattern(part, &GREEN) {
         cubes.green = cubes.green + green;
+        if min.green < green {
+          min.green = green;
+        }
       }
       if cubes.blue > blue_max || cubes.red > red_max || cubes.green > green_max {
         valid = false;
       }
     }
+    let power = min.blue * min.red * min.green;
+    sum_powers = sum_powers + power;
     if valid {
       sum = sum + check_pattern(&line, &GAME).unwrap();
       valid_games = valid_games + 1;
     }
   }
-  println!("Game sum: {} - valid: {}", sum, valid_games);
+  println!(
+    "Game sum: {} - powers: {} - valid: {}",
+    sum, sum_powers, valid_games
+  );
   Ok(())
 }
