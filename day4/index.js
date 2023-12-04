3a66aafebb7df2e8d02e76b4c7fc383177ffc279
mike12345567 <me@michaeldrury.co.uk>
2023-12-04 18:00:41 +0000

    Part 2 day 4.

--- day4/index.js
+++ day4/index.js
@@ -9,9 +9,11 @@ function run() {
   const lines = input.split("\n")
 
   let sum = 0
+  let copies = {}
   for (let line of lines) {
     const [winning, mine] = line.split("|")
     const [card, numbers] = winning.split(":")
+    const cardNumber = parseInt(fixSpaces(card).split(" ")[1])
     const winningNumbers = fixSpaces(numbers).split(" ")
     const myNumbers = fixSpaces(mine).split(" ")
     const filtered = myNumbers.filter(num => winningNumbers.indexOf(num) !== -1)
@@ -23,7 +25,19 @@ function run() {
     } else {
       sum += Math.pow(2, filtered.length - 1)
     }
+    // count how many copies win also
+    const wins = 1 + (copies[cardNumber] || 0)
+    // add copies
+    for (let i = 1; i < filtered.length + 1; i++) {
+      if (!copies[cardNumber + i]) {
+        copies[cardNumber + i] = 1 * wins
+      } else {
+        copies[cardNumber + i] += 1 * wins
+      }
+    }
   }
+  const cardCount = lines.length + Object.values(copies).reduce((prev, el) => prev + el, 0)
+  console.log(`Card count is: ${cardCount}`)
   console.log(`Sum is: ${sum}`)
 }
 
