3efbc1aced78fd595ac4b8e9b7671a7dcc752593
mike12345567 <me@michaeldrury.co.uk>
2023-12-08 18:50:26 +0000

    Day 8 part 2 solution.

--- day8/index.js
+++ day8/index.js
@@ -2,29 +2,70 @@ const fs = require("fs")
 
 const REMOVE_BRACKETS_REGEX = /\(*\)*/g
 const END = "ZZZ"
+const PART_2 = true
 
-function run() {
-  const MAP = {}
-  const lines = fs.readFileSync("./input.txt", "utf8").split("\n")
-  const instructions = lines.shift()
-  for (let line of lines) {
-    if (line === "") {
-      continue
-    }
-    const [node, LR] = line.split(" = ")
-    const [L, R] = LR.replaceAll(REMOVE_BRACKETS_REGEX, "").split(", ")
-    MAP[node] = { L, R }
+function lcm(inputs) {
+  function gcd(a, b) {
+    return b === 0 ? a : gcd(b, a % b)
   }
+  function lcm(a, b) {
+    return (a * b) / gcd(a, b)
+  }
+  let result = inputs[0]
+  for (let i = 1; i < inputs.length; i++) {
+    result = lcm(result, inputs[i])
+  }
+  return result
+}
+
+function part1Solver(instructions, map) {
   let current = "AAA", stepCount = 0, charIdx = 0
   do {
-    current = MAP[current][instructions.charAt(charIdx)]
-    charIdx++
+    current = map[current][instructions.charAt(charIdx++)]
     if (charIdx >= instructions.length) {
       charIdx = 0
     }
     stepCount++
   } while (current !== END)
-  console.log(`Steps to reach ZZZ: ${stepCount}`)
+  return stepCount
+}
+
+function part2Solver(instructions, starts, map) {
+  let pathCounts = []
+  for (let start of starts) {
+    let found = 0, count = 0, charIdx = 0, current = start
+    do {
+      const instruction = instructions.charAt(charIdx++)
+      if (charIdx >= instructions.length) {
+        charIdx = 0
+      }
+      found = 0
+      current = map[current][instruction]
+      count++
+    } while (!current.endsWith("Z"))
+    pathCounts.push(count)
+  }
+  return lcm(pathCounts)
+}
+
+function run() {
+  const MAP = {}
+  const lines = fs.readFileSync("./input.txt", "utf8").split("\n")
+  const instructions = lines.shift()
+  const starts = []
+  for (let line of lines) {
+    if (line === "") {
+      continue
+    }
+    const [node, LR] = line.split(" = ")
+    if (node.endsWith("A")) {
+      starts.push(node)
+    }
+    const [L, R] = LR.replaceAll(REMOVE_BRACKETS_REGEX, "").split(", ")
+    MAP[node] = { L, R }
+  }
+  const count = !PART_2 ? part1Solver(instructions, MAP) : part2Solver(instructions, starts, MAP)
+  console.log(`Steps to reach end: ${count}`)
 }
 
 run()
